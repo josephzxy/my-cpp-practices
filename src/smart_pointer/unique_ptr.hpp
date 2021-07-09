cdf8e0822b530bdf208b8013e47dfa392481219f
josephzxy <josephchuh7@gmail.com>
2021-07-09 13:13:58 +0800

    file📦:  Add unique_ptr

---
+++ src/smart_pointer/unique_ptr.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#include "smart_ptr_base.hpp"
+
+template <typename T>
+class unique_ptr : public smart_ptr<T>
+{
+    /*
+    unique_ptr fully utilises RAII and will free the resources
+    on its destruction(e.g. when it reaches the end of the scope)
+    It's important that at any time there should be ONLY one unique_ptr
+    for a piece of resource on the heap or it may result in releasing
+    the same chunk of memory multiple times.
+    */
+public:
+    explicit unique_ptr(T* ptr = nullptr)
+        : smart_ptr<T>(ptr) {}
+
+    // the copy constructor will be marked as delete
+    // implicitly if the move constructor is presented.
+    // It does no harm to explicitly mark it as delete here.
+    unique_ptr(unique_ptr& other) = delete;
+    // It's a common practice to keep both the template
+    // and the non-template version
+    template <typename U>
+    unique_ptr<T>(unique_ptr<U>&& other)
+    { this->ptr_ = other.release(); }
+
+    unique_ptr(unique_ptr<T>&& other)
+    { this->ptr_ = other.release(); }
+
+    ~unique_ptr() { delete this->ptr_; }
+
+    unique_ptr<T>& operator=(unique_ptr<T> other);
+
+    T* release() noexcept;
+
+    void swap(unique_ptr<T>& other) noexcept;
+};
+
+#include "unique_ptr.ipp"
---
+++ src/smart_pointer/unique_ptr.ipp
@@ -0,0 +1,34 @@
+#pragma once
+#include <utility>
+#include "unique_ptr.hpp"
+
+template <typename T>
+unique_ptr<T>& unique_ptr<T>::operator=(unique_ptr<T> other)
+{
+    /*
+    Note that we are taking value instead of reference
+    in the argument list so as to create a temporary
+    local variable either by move or copy,
+
+    swap is noexcept. The only place where errors may occur
+    is contructing the new temporary object.
+    The advantage of this practice is that "this" won't get
+    affected if constructing the new temporary object fails.
+    */
+    other.swap(*this);
+    return *this;
+}
+
+template <typename T>
+T* unique_ptr<T>::release() noexcept
+{
+    T* p = this->ptr_;
+    this->ptr_ = nullptr;
+    return p;
+}
+
+template <typename T>
+void unique_ptr<T>::swap(unique_ptr<T>& other) noexcept
+{
+    std::swap(this->ptr_, other.ptr_);
+}
